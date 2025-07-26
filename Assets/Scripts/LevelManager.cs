3aa11474185b71267c7cc0e4f04a93a835813118
AlirieGray <alirie.gray@gmail.com>
2025-07-25 19:06:04 -0700

    wip

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,20 +1,48 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     private int cyclesPlayed;
     private List<string> levelOrder;
+    private int currentLevel;
     void Start()
     {
         cyclesPlayed = 0;
         DontDestroyOnLoad(gameObject);
-        levelOrder = new List<string> {}
+        // TODO: update with resource collection level
+        levelOrder = new List<string>
+        {
+            "MainMenu", "GatherResources", "Launch", "Placeholder", "GameOver"
+        };
     }
 
     public void LoadNextLevel()
     {
+        if (currentLevel == 3) { 
+            cyclesPlayed++;
+            
+            if (cyclesPlayed == 6)
+            {
+                currentLevel = 3;
+                SceneManager.LoadScene("GameOver");
+            } else
+            {
+                currentLevel = 1;
+                SceneManager.LoadScene("GatherResources");
+            }
+        } else 
+        {
+            currentLevel++;
+            SceneManager.LoadScene(levelOrder[currentLevel]);
+        }
+    }
 
+    public void LoadMainMenu()
+    {
+        currentLevel = 0;
+        SceneManager.LoadScene(levelOrder[currentLevel]);
     }
 
     public int GetCurrentCycle()
