f48694b55901a81f97cbbbe3de77ea09a40a29c2
AlirieGray <alirie.gray@gmail.com>
2025-07-25 23:29:35 -0700

    wip

---
+++ Assets/FireworksTrigger.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TriggerScript : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+    void OnEnable()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnParticleTrigger()
+    {
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+        // iterate through the particles which entered the trigger and make them red
+        for (int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            p.velocity = new Vector3(p.velocity.x / 112, p.velocity.y / 112, p.velocity.z / 112);
+            enter[i] = p;
+        }
+
+        // iterate through the particles which exited the trigger and make them green
+        for (int i = 0; i < numExit; i++)
+        {
+            ParticleSystem.Particle p = exit[i];
+            p.velocity = new Vector3(p.velocity.x /112, p.velocity.y / 112, p.velocity.z / 112);
+            exit[i] = p;
+        }
+
+        // re-assign the modified particles back into the particle system
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+    }
+}
