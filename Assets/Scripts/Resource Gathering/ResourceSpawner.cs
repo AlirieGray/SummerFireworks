7c5ac4cf17f22cba8e0498e7a49480dc72e066fd
Choobyshroom <jwkcrafter1@gmail.com>
2025-07-26 22:31:29 -0700

    Yeah...

--- Assets/Scripts/Resource Gathering/ResourceSpawner.cs
+++ Assets/Scripts/Resource Gathering/ResourceSpawner.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public class ResourceSpawner : MonoBehaviour
 {
     Vector2 screenSize;
+    public int extraSpawns = 3;
 
     private void Start()
     {
@@ -15,6 +16,22 @@ public class ResourceSpawner : MonoBehaviour
                 if (res.name != "Blunder")
                     SpawnResource(res);
             }
+
+        int randomSpawnAmount = 0;
+
+        for (int i = GameManager.manager.score; i > 0; i -= 1000)
+            randomSpawnAmount++;
+
+        for(int i = randomSpawnAmount + extraSpawns; i > 0; i--)
+        {
+            ResourceScriptableObject rolledResource = GameManager.manager.resources[Random.Range(0, GameManager.manager.resources.Count)];
+            while (rolledResource.name == "Blunder")
+                rolledResource = GameManager.manager.resources[Random.Range(0, GameManager.manager.resources.Count)];
+
+            SpawnResource(rolledResource);
+        }
+
+        SpawnResource(GameManager.manager.resourceNames["VolitileCrystals"]);
     }
 
     void SpawnResource(ResourceScriptableObject res)
@@ -25,6 +42,8 @@ public class ResourceSpawner : MonoBehaviour
         SpriteRenderer resourceSpriteRenderer = resource.AddComponent<SpriteRenderer>();
 
         resourceSpriteRenderer.sprite = res.overrideSprite;
+        resourceSpriteRenderer.sortingLayerName = "Sprites";
+
         lootScript.resourceType = res;
 
         resource.transform.position = new Vector2(Random.Range(-screenSize.x, screenSize.x), Random.Range(-screenSize.y, screenSize.y));
